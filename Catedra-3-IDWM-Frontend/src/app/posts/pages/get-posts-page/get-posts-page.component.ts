03bc607cc74518cda561aae72d3022192dbd4e14
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-01-08 00:44:10 -0300

    feat: create folder structure and components.

---
+++ Catedra-3-IDWM-Frontend/src/app/posts/pages/get-posts-page/get-posts-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-posts-page',
+  imports: [],
+  templateUrl: './get-posts-page.component.html',
+  styleUrl: './get-posts-page.component.css'
+})
+export class GetPostsPageComponent {
+
+}
