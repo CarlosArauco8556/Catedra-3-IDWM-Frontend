df00386f189e0575f70eae8342866a6d02b3f92c
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-01-08 23:32:57 -0300

    feat: add navbar.

--- Catedra-3-IDWM-Frontend/src/app/_shared/components/navbar/navbar.component.ts
+++ Catedra-3-IDWM-Frontend/src/app/_shared/components/navbar/navbar.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
+  selector: 'navbar',
+  imports: [HttpClientModule, CommonModule],
+  providers: [AuthService],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  authService: AuthService = inject(AuthService);
+  toastService: ToastService = inject(ToastService)
+  postIconIsHovered = false;
+  auhtIconIsHovered = false;
+  menuOpenPosts = false;
+  menuOpenAuth = false;
+  errors: string[] = [];
 
+  constructor(private router: Router){}
+  
+  toggleMenuPosts(): void{
+    this.menuOpenPosts = !this.menuOpenPosts;
+  }
+  toggleMenuAuth(): void {
+    this.menuOpenAuth = !this.menuOpenAuth;
+  }
+
+  goToPage(page: string){
+    switch(page){
+      case 'Login':
+        this.router.navigate(['/login'])
+        break;
+      case 'Register':
+        this.router.navigate(['/register'])
+        break;
+      case 'GetPosts':
+        this.router.navigate(['/get-posts'])
+        break;
+      case 'CreatePost':
+        this.router.navigate(['/create-post'])
+        break;
+    }
+  }
+
+  async logOut(){
+    try {
+      const response = await this.authService.logout();
+      if(response){
+        this.toastService.success("Cierre de sesión exitoso");
+        this.router.navigate(['/home']);
+      }else{
+        this.errors = this.authService.errors;
+        const lastError = this.errors[this.errors.length - 1];
+        this.toastService.error(lastError || "No se pudo cerrar sesión");
+      }
+    }catch (error: any){
+      if(error instanceof HttpErrorResponse)
+      {
+        const errorMessage = 
+          typeof error.error === 'string' ? error.error : error.error.message || error.statusText || 'No se pudo cerrar sesión';
+        this.errors.push(errorMessage);
+        this.toastService.error(errorMessage || 'No se pudo cerrar sesión');
+      }
+      console.log('Error in logout', error);
+    }
+  }
 }
